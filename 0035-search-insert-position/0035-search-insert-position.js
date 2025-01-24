9042fde13bb67b5c6412f07b09dd30ef8bc18e14
Simrit (Simmy) Sidhu <sks293@cornell.edu>
2025-01-24 12:56:09 -0500

    Time: 0 ms (100%), Space: 50.5 MB (6.45%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -14,3 +14,25 @@ var searchInsert = function(nums, target) {
         return newFoundTarget;
     }
 };
+
+//More efficient way:
+//Use a binary search with Math.floor
+//https://www.hellointerview.com/learn/code/binary-search/overview
+//you are looking from the middle
+
+// var searchInsert=function(nums, target){
+//     let low = 0
+//     let high = nums.length
+//     let mid;
+//     while(low>high){
+//         mid = Math.floor(high-low)/2+low
+//         if (target>nums[mid]){
+//             low=mid+1
+//         }
+//         else if (target==nums[mid]){
+//             return mid
+//         }
+//         else high = mid
+//     }
+//     return low
+// }
