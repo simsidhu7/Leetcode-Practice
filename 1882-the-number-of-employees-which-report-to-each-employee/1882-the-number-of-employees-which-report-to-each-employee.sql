2157e57bb29787343a23c8048b7774e422207bb7
Simrit (Simmy) Sidhu <sks293@cornell.edu>
2025-02-04 00:01:02 -0500

    Time: 2624 ms (5%), Space: 0B (100%) - LeetHub

--- 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
@@ -1,10 +1,5 @@
 # Write your MySQL query statement below
 
--- SELECT employee_id, name, COUNT(reports_to) AS reports_count, ROUND(AVG(age)) AS average_age
--- FROM Employees
--- WHERE employee_id IN (SELECT reports_to FROM Employees WHERE reports_to IS NOT NULL)
--- ORDER BY employee_id
-
 SELECT e.employee_id, 
        e.name, 
        (SELECT COUNT(*) FROM Employees WHERE reports_to = e.employee_id) AS reports_count,
@@ -12,3 +7,8 @@ SELECT e.employee_id,
 FROM Employees e
 WHERE employee_id IN (SELECT reports_to FROM Employees WHERE reports_to IS NOT NULL)
 ORDER BY employee_id;
+
+-- Another way to do it:
+-- select e.employee_id,e.name, COUNT(sec.employee_id) as reports_count, ROUND(AVG(sec.age)) as average_age 
+-- from Employees e inner join Employees sec on e.employee_id = sec.reports_to 
+-- group by e.employee_id order by e.employee_id
