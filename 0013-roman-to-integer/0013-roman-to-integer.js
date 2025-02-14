eb52cad30777a579c0a28ebf4c4188526fc37c27
Simrit (Simmy) Sidhu <sks293@cornell.edu>
2025-02-14 12:36:04 -0500

    Time: 7 ms (52.42%), Space: 54.8 MB (73.16%) - LeetHub

---
+++ 0013-roman-to-integer/0013-roman-to-integer.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const romanValues = {
+        'I': 1, 'V': 5, 'X': 10, 'L': 50,
+        'C': 100, 'D': 500, 'M': 1000
+    };
+
+    let total = 0;
+    let prev = 0;
+
+    //From right to left
+    for (let i = s.length - 1; i >= 0; i--) {
+        let current = romanValues[s[i]];
+
+        if (current < prev) {
+            // Subtract if a smaller value before a larger one.
+            total -= current; 
+        } else {
+            // Otherwise add it.
+            total += current; 
+        }
+// Update the previous value.
+        prev = current; 
+    }
+    return total;
+};
